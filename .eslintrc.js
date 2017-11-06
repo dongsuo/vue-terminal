ee40d44eda4ce2bfea5f88656960898e6ad296e6
dongsuo <xuxiaofei1.0@outlook.com>
2017-11-06 15:59:56 +0800

    fix:LF

--- .eslintrc.js
+++ .eslintrc.js
@@ -189,7 +189,7 @@ module.exports = {
         'keyword-spacing': 'error',
 
         // enforce consistent linebreak style
-        'linebreak-style': 'error',
+        'linebreak-style': ['error', 'windows'],
 
         // require or disallow newlines around directives
         'lines-around-directive': 'error',

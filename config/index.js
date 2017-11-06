2d7ef3ab8729a3fbd2f223df6328a6d208cd8941
dongsuo <xuxiaofei1.0@outlook.com>
2017-11-06 16:35:35 +0800

    fix:relative path

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 
 'use strict'
+
 // Template version: 1.1.3
 // see http://vuejs-templates.github.io/webpack for documentation.
 
@@ -11,7 +12,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.

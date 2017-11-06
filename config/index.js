acaee8b1d0ce44e28c844a728e45f492244733a9
dongsuo <xuxiaofei1.0@outlook.com>
2017-11-06 21:36:47 +0800

    chore:change output folder and publish

--- config/index.js
+++ config/index.js
@@ -9,8 +9,8 @@ const path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../docs/index.html'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
     productionSourceMap: true,
